401ad142c6fd888b1a6782a5a9eeccfe20046a3b
Lars Helge Scheel <larshelge.scheel@gmail.com>
2020-06-25 10:39:04 +0200

    Confusing hint

--- build_shared.sh
+++ build_shared.sh
@@ -2,5 +2,5 @@
 
 scons platform=x11 use_llvm=yes use_lld=yes boid_particles_shared=yes bin/libboid_particles.x11.tools.64.llvm.so -j4
 
-# first time around
+# first time around also execute this
 #scons platform=x11 use_llvm=yes use_lld=yes boid_particles_shared=yes -j4
