69e42a477e9f9df3781cef9551152ea0be268b72
Lars Helge Scheel <larshelge.scheel@gmail.com>
2020-06-30 12:38:36 +0200

    Animation is done with custom[2], remove members of particle

--- modules/boid_particles/boid.h
+++ modules/boid_particles/boid.h
@@ -42,27 +42,14 @@ public:
 
 			p.seed = Math::rand();
 
-			p.angle_rand = Math::randf();
-			p.scale_rand = Math::randf();
-			p.hue_rot_rand = Math::randf();
-			p.anim_offset_rand = Math::randf();
-
-
             p.velocity = Vector2();
-            p.rotation = 0.0;
-
 			p.custom[0] = 0.0; // unused
 			p.custom[1] = 0.0; // phase [0..1]
 			p.custom[2] = 0.0;
 			p.custom[3] = 0.0;
 			p.transform = Transform2D();
 			p.time = 0;
-            p.lifetime = 10000.0;
-			p.base_color = Color(1, 1, 1, 1);
             p.color = Color(1,1,1,1);
-
-            p.transform.elements[0] *= 10.0;
-            p.transform.elements[1] *= 10.0;
             p.transform[2] = Vector2(i * 100, 0);
 
         }
@@ -81,6 +68,8 @@ public:
 
             p.transform[2][0] = std::cos(i + t) * 100.0;
             p.transform[2][1] = std::sin(i + t) * 100.0;
+
+            p.custom[2] = fmod(t, 1.0);
 
         }
 
--- modules/boid_particles/particle.h
+++ modules/boid_particles/particle.h
@@ -12,17 +12,9 @@ struct Particle {
     Transform2D transform;
     Color color;
     float custom[4];
-    float rotation;
     Vector2 velocity;
     bool active;
-    float angle_rand;
-    float scale_rand;
-    float hue_rot_rand;
-    float anim_offset_rand;
     float time;
-    float lifetime;
-    Color base_color;
-
     uint32_t seed;
 
 
@@ -33,16 +25,9 @@ public:
         std::cout << "Transform2D transform; " << transform << std::endl;
         std::cout << "Color color; " << color << std::endl;
         std::cout << "float custom[4]; " << custom[0] << ", " << custom[1] << ", " << custom[2] << ", " << custom[3] <<  std::endl;
-        std::cout << "float rotation; " << rotation << std::endl;
         std::cout << "Vector2 velocity; " << velocity << std::endl;
         std::cout << "bool active; " << active << std::endl;
-        std::cout << "float angle_rand; " << angle_rand << std::endl;
-        std::cout << "float scale_rand; " << scale_rand << std::endl;
-        std::cout << "float hue_rot_rand; " << hue_rot_rand << std::endl;
-        std::cout << "float anim_offset_rand; " << anim_offset_rand << std::endl;
         std::cout << "float time; " << time << std::endl;
-        std::cout << "float lifetime; " << lifetime << std::endl;
-        std::cout << "Color base_color; " << base_color << std::endl;
         std::cout << "uint32_t seed; " << seed << std::endl;
 
     }
