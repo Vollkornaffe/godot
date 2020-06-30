f13d728e33082e9c3a517853f3ccfa4a76a0d12b
Lars Helge Scheel <larshelge.scheel@gmail.com>
2020-06-30 12:38:09 +0200

    Remove from the build & register

--- modules/boid_particles/register_types.cpp
+++ modules/boid_particles/register_types.cpp
@@ -3,13 +3,11 @@
 #include "register_types.h"
 
 #include "core/class_db.h"
-#include "boid_particles.h"
 #include "sdf_texture_rect.h"
 #include "boid.h"
 #include "passive_particles.h"
 
 void register_boid_particles_types() {
-    ClassDB::register_class<BoidParticles>();
     ClassDB::register_class<SDFTextureRect>();
     ClassDB::register_class<Boid>();
     ClassDB::register_class<PassiveParticles>();
